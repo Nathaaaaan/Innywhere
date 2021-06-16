da3de427659d3d8af5cfeb48da5cc5b824779792
Rido R <ridorachmatan@gmail.com>
2021-06-16 09:02:31 +0700

    Icontext edited.

--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -6,7 +6,10 @@ import ImageHeroFrame from "assets/images/img-hero-frame.jpg";
 import Button from "elements/Button";
 export default function Hero(props) {
   function ExploreNow() {
-    window.scrollTo();
+    window.scrollTo({
+      // top: (props.refFindCities.current.offsetTop = 30),
+      // behavior: "smooth",
+    });
   }
 
   return (
@@ -18,10 +21,10 @@ export default function Hero(props) {
             hundreds of different cultures.
           </h1>
           <h6 className="mb-5 font-weight-light text-gray-500 w-10">
-            Location
+            I don't know what to put here, I'll come back later!
           </h6>
           <Button
-            className="btn-px-5"
+            className="btn px-5"
             type="link"
             hasShadow
             isPrimary
@@ -29,6 +32,27 @@ export default function Hero(props) {
           >
             Explore Me
           </Button>
+
+          <div className="row mt-5">
+            <div className="col-auto" style={{ marginRight: 35 }}>
+              <h6 className="mt-3">
+                {props.data.members} <br />
+                <span className="text-gray-500 font-weight-light">members</span>
+              </h6>
+            </div>
+            <div className="col-auto" style={{ marginRight: 35 }}>
+              <h6 className="mt-3">
+                {props.data.cities} <br />
+                <span className="text-gray-500 font-weight-light">cities</span>
+              </h6>
+            </div>
+            <div className="col-auto">
+              <h6 className="mt-3">
+                {props.data.events} <br />
+                <span className="text-gray-500 font-weight-light">events</span>
+              </h6>
+            </div>
+          </div>
         </div>
       </div>
     </section>
